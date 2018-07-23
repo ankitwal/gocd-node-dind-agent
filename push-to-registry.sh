f1731362bf830e8217899ac2db0d45eb831fa85c
Ankit Wal <ankit.wal@gmail.com>
2018-07-23 14:55:08 +0800

    force http push to internal registry

--- push-to-registry.sh
+++ push-to-registry.sh
@@ -1,2 +1,2 @@
 docker tag custom-node-build-agent ${REGISTRY_NAME}/${PROJECT_ID}/custom-node-build-agent
-docker push ${REGISTRY_NAME}/${PROJECT_ID}/custom-node-build-agent
+docker push --insecure-registry ${REGISTRY_NAME}/${PROJECT_ID}/custom-node-build-agent
